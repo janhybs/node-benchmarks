263d12a7b565bf3fea9a01a7b150e2f13f7aa884
Jan_Hybs <jan.hybs@tul.cz>
2016-04-04 13:33:53 +0200

    Add io tests, unify test iface
     - add tmp files to ignore

--- src/memory.cc
+++ src/memory.cc
@@ -9,10 +9,20 @@
 #include "libs/json.hpp"
 
 #define KB  1024
-#define MB  1024 * 1024
+#define MB  1048576
 #define REP MB
 #define OFFSET 256
-#define ARR_SIZE MB *100
+#define ARR_SIZE (MB * 100)
+
+#define GIGA  1.0e+9
+#define MEGA  1.0e+6
+#define KILO  1.0e+3
+#define MILI  1.0e-3
+#define MICRO 1.0e-6
+#define NANO  1.0e-9
+
+#define CHAR_SIZE sizeof(char)
+#define INT_SIZE  sizeof(int)
 
 using namespace std;
 using json = nlohmann::json;
@@ -22,13 +32,13 @@ protected:
     chrono::high_resolution_clock::time_point _start;
     chrono::high_resolution_clock::time_point _stop;
 public:
-    chrono::duration<double, micro> duration;
+    chrono::duration<double, nano> duration;
     void start() {
         this->_start = std::chrono::high_resolution_clock::now();
     }
     void stop() {
         this->_stop = std::chrono::high_resolution_clock::now();
-        this->duration = chrono::duration_cast<chrono::microseconds>(this->_stop - this->_start);
+        this->duration = chrono::duration_cast<chrono::nanoseconds>(this->_stop - this->_start);
     }
 };
 
@@ -45,6 +55,9 @@ void printf_debug(const char * fmt, ...) {
 void cpu_test(json &results, int repetition = REP) {
     Timer timer;
     int i, sum;
+    
+    printf("-- running CPU REG test with %d k repetition\n", repetition / KB);
+    
     //-------------------------------------------------------
     timer.start();
         for (i = 0; i < repetition; i++) {
@@ -54,7 +67,6 @@ void cpu_test(json &results, int repetition = REP) {
     //-------------------------------------------------------
     results["duration"] = timer.duration.count();
     results["reps"] = repetition;
-    printf("cpu   : fin             \n");
 }
 
 
@@ -62,9 +74,13 @@ template <int N, int M>
 void cpu_test_r(json &results, int (&arr)[M], int (&sizes)[N], int repetition = REP) {
     Timer timer;
     int i, j, mod, sum;
-    const int i_max = sizeof(sizes)/sizeof(int);
+    const int i_max = sizeof(sizes) / INT_SIZE;
+    
+    printf("-- running CPU R   test with %d k repetition\n", repetition / KB);
+    
     for (i = 0; i < i_max; i++) {
         mod = sizes[i] - 1;
+        printf_debug("buffer size %9d kB...", sizes[i] / KB);
         //-------------------------------------------------------
         timer.start();
             for (j = 0; j < repetition; j++)  {
@@ -72,13 +88,11 @@ void cpu_test_r(json &results, int (&arr)[M], int (&sizes)[N], int repetition = 
             }
         timer.stop();
         //-------------------------------------------------------
-        results["duration"][to_string(sizes[i]/1024*sizeof(int))] = timer.duration.count();
-        printf_debug("cpu r:  %d -> %d", sizes[i]/1024*sizeof(int), timer.duration.count());
+        results["duration"][to_string(sizes[i]/1024 * INT_SIZE)] = timer.duration.count();
     }
     results["reps"] = repetition;
-    results["size"] = sizeof(arr)/sizeof(int);
+    results["size"] = sizeof(arr) / INT_SIZE;
     results["sum"] = sum;
-    printf("cpu r : fin             \n");
 }
 
 
@@ -86,9 +100,13 @@ template <int N, int M>
 void cpu_test_w(json &results, int (&arr)[M], int (&sizes)[N], int repetition = REP) {
     Timer timer;
     int i, j, mod;
-    const int i_max = sizeof(sizes)/sizeof(int);
+    const int i_max = sizeof(sizes) / INT_SIZE;
+    
+    printf("-- running CPU W   test with %d k repetition\n", repetition / KB);
+    
     for (i = 0; i < i_max; i++) {
         mod = sizes[i] - 1;
+        printf_debug("buffer size %9d kB...", sizes[i] / KB);
         //-------------------------------------------------------
         timer.start();
             for (j = 0; j < repetition; j++) {
@@ -96,12 +114,10 @@ void cpu_test_w(json &results, int (&arr)[M], int (&sizes)[N], int repetition = 
             }
         timer.stop();
         //-------------------------------------------------------
-        results["duration"][to_string(sizes[i]/1024*sizeof(int))] = timer.duration.count();
-        printf_debug("cpu w:  %d -> %d", sizes[i]/1024*sizeof(int), timer.duration.count());
+        results["duration"][to_string(sizes[i]/1024 * INT_SIZE)] = timer.duration.count();
     }
     results["reps"] = repetition;
-    results["size"] = sizeof(arr)/sizeof(int);
-    printf("cpu w : fin             \n");
+    results["size"] = sizeof(arr) / INT_SIZE;
 }
 
 
@@ -109,9 +125,13 @@ template <int N, int M>
 void cpu_test_rw(json &results, int (&arr)[M], int (&sizes)[N], int repetition = REP) {
     Timer timer;
     int i, j, mod, sum;
-    const int i_max = sizeof(sizes)/sizeof(int);
+    const int i_max = sizeof(sizes) / INT_SIZE;
+    
+    printf("-- running CPU R/W test with %d k repetition\n", repetition / KB);
+
     for (i = 0; i < i_max; i++) {
         mod = sizes[i] - 1;
+        printf_debug("buffer size %9d kB...", sizes[i] / KB);
         //-------------------------------------------------------
         timer.start();
             for (j = 0; j < repetition; j++) {
@@ -120,56 +140,128 @@ void cpu_test_rw(json &results, int (&arr)[M], int (&sizes)[N], int repetition =
             }
         timer.stop();
         //-------------------------------------------------------
-        results["duration"][to_string(sizes[i]/1024*sizeof(int))] = timer.duration.count();
-        printf_debug("cpu rw: %d -> %d", sizes[i]/1024*sizeof(int), timer.duration.count());
+        results["duration"][to_string(sizes[i]/1024 * INT_SIZE)] = timer.duration.count();
     }
     results["reps"] = repetition;
-    results["size"] = sizeof(arr)/sizeof(int);
-    printf("cpu rw: fin             \n");
+    results["size"] = sizeof(arr) * INT_SIZE;
 }
 
-template <int N, int M>
-void cpu_test_read_write_split(json &results, int (&arr)[M], int (&sizes)[N], int repetition = REP) {
-    Timer timer;
-    int i, j, mod, sum;
-    const int i_max = sizeof(sizes)/sizeof(int);
-    for (i = 0; i < i_max; i++) {
-        mod = sizes[i] - 1;
-        //-------------------------------------------------------
-        timer.start();
-            for (j = 0; j < repetition; j++) {
-                arr[(j * OFFSET) & mod] = 1;
-            }
-            for (j = 0; j < repetition; j++) {
-                sum += arr[(j * OFFSET) & mod];
-            }
-        timer.stop();
-        //-------------------------------------------------------
-        results["duration"][to_string(sizes[i]/1024*sizeof(int))] = timer.duration.count();
-    }
-    results["reps"] = repetition;
-    results["size"] = sizeof(arr)/sizeof(int);
-    results["sum"] = sum;
-}
-
-void io_test_r(json &results, int repetition) {
-    Timer timer;
-    static char buffer[] = {'x' , 'y' , 'z', 'w'};
-    FILE* f = fopen("tmp_file.bin", "wb");
+template <int N>
+void io_test_rw(json &results, int (&sizes)[N], int file_size, const int buffer_size = 1 * MB) {
+    char *buffer = new char[buffer_size];
+    char *read_buffer;
+    Timer timer_r, timer_w;
+    ofstream write_stream;
+    ifstream read_stream;
+    string fname;
+    int repetition, i, j, k, i_max;
+    i_max = sizeof(sizes) / INT_SIZE;
     
-    //-------------------------------------------------------
-    timer.start();
-    for (int i = 0; i < repetition; i++) {
-        //Some calculations to fill a[]
-        fwrite (buffer , sizeof(char), sizeof(buffer), f);
+    printf("-- running  IO R/W bandwidth test with %1.3f MB file size\n", (float)file_size / MB);
+    
+    for (i = 0; i < i_max; i++) {
+        printf_debug("(%2d/%2d) buffer size %9.3f kB...", i + 1, i_max, (float)sizes[i] / KB);
+        repetition = file_size / (sizes[i] * CHAR_SIZE);
+        fname = "tmp_file_" + to_string(sizes[i]) + ".tmp";
+        read_buffer = new char[sizes[i]];
+        
+        // write file
+        write_stream.open(fname.c_str(), ios::binary | ios::out);
+        //-------------------------------------------------------
+        timer_w.start();
+            for (int j = 0; j < repetition; j++) {
+                write_stream.write (buffer, sizes[i]);
+            }
+        timer_w.stop();
+        //-------------------------------------------------------
+        if (write_stream.fail()) {
+            // buffer may be too big so ignore this 
+            write_stream.close();    
+            remove(fname.c_str());
+            continue;
+        } else {
+            write_stream.close();    
+        }
+        
+        // read file
+        read_stream.open(fname.c_str(), ios::binary | ifstream::in);
+        //-------------------------------------------------------
+        timer_r.start();
+            for (int j = 0; j < repetition; j++) {
+                read_stream.read (read_buffer, sizes[i]);
+            }
+        timer_r.stop();
+        //-------------------------------------------------------
+        if (read_stream.fail()) {
+            // buffer may be too big so ignore this 
+            read_stream.close();    
+            remove(fname.c_str());
+            continue;
+        } else {
+            read_stream.close();    
+        }
+        
+        remove(fname.c_str());
+        
+        results["write"]["duration"][to_string(sizes[i] * CHAR_SIZE)] = timer_w.duration.count() * NANO;
+        results["write"]["eff"][to_string(sizes[i] * CHAR_SIZE)] = ((repetition * sizes[i])/timer_w.duration.count() / NANO) / MEGA;
+        
+        results["read"]["duration"][to_string(sizes[i] * CHAR_SIZE)] = timer_r.duration.count() * NANO;
+        results["read"]["eff"][to_string(sizes[i] * CHAR_SIZE)] = ((repetition * sizes[i])/timer_r.duration.count() / NANO) / MEGA;
     }
-    timer.stop();
-    //-------------------------------------------------------
+    results["size"] = file_size;
+}
 
-    fclose(f);
-    results["duration"] = timer.duration.count();
-    results["eff"] = repetition/timer.duration.count();
-    printf("io r  : fin             \n");
+void io_test_many(json &results, int no_files = KILO) {
+    Timer timer_r, timer_w, timer_d;
+    ofstream write_stream;
+    ifstream read_stream;
+    string fname;
+    int i, n;
+    
+    printf("-- running IO  R/W latency test with %d files\n", no_files);
+    
+    printf_debug("writing test...");
+    //-------------------------------------------------------
+    timer_w.start();
+    for (i = 0; i < no_files; i++) {
+        fname = "tmp_file_" + to_string(i) + ".tmp";
+        write_stream.open(fname.c_str(), ios::binary | ios::out);
+        write_stream << 0;
+        write_stream.close();
+    }
+    timer_w.stop();
+    //-------------------------------------------------------
+    
+    
+    printf_debug("reading test...");
+    //-------------------------------------------------------
+    timer_r.start();
+    for (i = 0; i < no_files; i++) {
+        fname = "tmp_file_" + to_string(i) + ".tmp";
+        read_stream.open(fname.c_str(), ios::binary | ios::in);
+        read_stream >> n;
+        read_stream.close();
+    }
+    timer_r.stop();
+    //-------------------------------------------------------
+    
+    
+    printf_debug("removing test...");
+    //-------------------------------------------------------
+    timer_d.start();
+    // remove files
+    for (i = 0; i < no_files; i++) {
+        fname = "tmp_file_" + to_string(i) + ".tmp";
+        remove(fname.c_str());
+    }
+    timer_d.stop();
+    //-------------------------------------------------------
+    
+    results["write"]["duration"] = timer_w.duration.count() * NANO;
+    results["read"]["duration"]  = timer_r.duration.count() * NANO;
+    results["del"]["duration"]   = timer_d.duration.count() * NANO;
+    results["count"] = no_files;
 }
 
 
@@ -185,52 +277,44 @@ int main(int argc,  char* argv[]) {
         // 4 * KB, 128 * KB, 8 * MB, 32 * MB
     };
     
+    // chunk size for testing
+    static int io_sizes[] = {
+        16, 32, 64, 128, 256, 512,
+        1 * KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 128 * KB,
+        256 * KB, 512 * KB, 1 * MB, 2 * MB, 4 * MB, 8 * MB
+    };
+    
     // create and randomize array
     printf_debug("creating array...         ");
     static int arr[ARR_SIZE];
     printf_debug("randomizing array...      ");
     for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
-        arr[i] = (i * 13778941) % 35;
+        arr[i] = (i * 13941) % 35; //13778941
     }
     printf_debug("running tests...         ");
     json results;
     
-    // cpu_test    (results["cpu"], rep_cnt * 100);
-    // cpu_test_r  (results["r"], arr, sizes, rep_cnt);
-    // cpu_test_w  (results["w"], arr, sizes, rep_cnt);
-    // cpu_test_rw (results["rw"], arr, sizes, rep_cnt);
-    io_test_r   (results["io"], rep_cnt);
     
-    printf_debug("generating output...    ");
+    Timer test_timer;
+    test_timer.start();
+    cpu_test     (results["cpu"]["reg"], rep_cnt * 100);
+    cpu_test_r   (results["cpu"]["read"], arr, sizes, rep_cnt);
+    cpu_test_w   (results["cpu"]["write"], arr, sizes, rep_cnt);
+    cpu_test_rw  (results["cpu"]["rw"], arr, sizes, rep_cnt);
+    io_test_rw   (results["io"]["band"], io_sizes, rep_cnt * 8);
+    io_test_many (results["io"]["lat"], (rep_cnt / REP) * KILO);
+    test_timer.stop();
+    
+    printf("---------------------------------\n");
+    printf("%-30s: %1.3f\n", "time taken", test_timer.duration.count() * NANO);
+    
+    printf_debug("generating output...    \n");
+    printf("                                ");
     cout << results.dump(true) << endl;
     if (argc >= 2) {
         ofstream ofs (argv[1]);
         ofs << results.dump(true) << endl;
     }
-    // results_read =  cpu_test_read(arr, sizes, REP * rep_cnt);
-    // results_write = cpu_test_write(arr, sizes, REP * rep_cnt);
-    // results_rw =    cpu_test_read_write(arr, sizes, REP * rep_cnt);
-    // results_cpu = cpu_test(REP * rep_cnt);
-    
-    // printf("%5s\t%5s\t%5s\t%5s\n", "size","read", "write", "r/w");
-    
-    
-    // store additional information about experiment
-    // j["reps"] = (int)(REP * rep_cnt);
-    // j["offset"] = OFFSET;
-    // j["arr_size"] = ARR_SIZE;
-    // for(int i = 0; i < sizeof(sizes)/sizeof(int); i++) {
-    //     int size = (int)((sizes[i]/1024)*sizeof(int));
-    //     printf("%d\t%1.3f\t%1.3f\t%1.3f\n", size, 
-    //     results_read[sizes[i]]/1000.0,
-    //     results_write[sizes[i]]/1000.0,
-    //     results_rw[sizes[i]]/1000.0);
-    //     
-    //     j["rw"]["data"][to_string(size)] = results_rw[sizes[i]];
-    // }
-    
-    // write results to cout and json file
-
     
     return 0;
 }
